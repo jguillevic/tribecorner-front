432c168258e5a05e7d0fc3e3d4e6cf9f1da73753
Jérôme GUILLEVIC <jguillevic@isagri.fr>
2023-11-04 10:04:46 +0100

    Revu de la card des listes de course.

--- src/app/shopping-list/ui/shopping-list-card/shopping-list-card.component.ts
+++ src/app/shopping-list/ui/shopping-list-card/shopping-list-card.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShoppingList } from '../../model/shopping-list.model';
+import { Observable, map, of } from 'rxjs';
+import { ItemShoppingList } from '../../model/item-shopping-list.model';
 
 @Component({
   selector: 'app-shopping-list-card',
@@ -12,8 +14,22 @@ import { ShoppingList } from '../../model/shopping-list.model';
   styles: [
   ]
 })
-export class ShoppingListCardComponent {
+export class ShoppingListCardComponent implements OnInit {
   @Input() public shoppingList: ShoppingList|undefined;
 
+  public notCheckedItemShoppingLists$: Observable<ItemShoppingList[]|undefined>|undefined;
+  public checkedItemShoppingLists$: Observable<ItemShoppingList[]|undefined>|undefined;
   public maxDisplayedItemsCount: number = 7;
+
+  public ngOnInit(): void {
+    this.notCheckedItemShoppingLists$ = of(this.shoppingList)
+    .pipe(
+      map(shoppingList => shoppingList?.items.filter(item => !item.isChecked))
+    );
+
+    this.checkedItemShoppingLists$ = of(this.shoppingList)
+    .pipe(
+      map(shoppingList => shoppingList?.items.filter(item => item.isChecked))
+    )
+  }
 }

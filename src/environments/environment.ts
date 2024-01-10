24668987fecf00ffe8aa70a203094918ad48a6b0
jguillevic <guillevicjerome@yahoo.fr>
2024-01-10 11:18:25 +0100

    Identation des fichiers environment.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,13 @@
 export const environment = {
   baseUrl: 'https://tribecorner.fr',
-    production: true,
-    apiUrl: 'https://api.tribecorner.fr/',
-    firebaseConfig : {
-        apiKey: "AIzaSyDuNMLuciC1INNmhzMEhlb5VWStT9J7Mu4",
-        authDomain: "familyhub-33bd5.firebaseapp.com",
-        projectId: "familyhub-33bd5",
-        storageBucket: "familyhub-33bd5.appspot.com",
-        messagingSenderId: "874615286570",
-        appId: "1:874615286570:web:bfff3469e09088487e4c53"
-      },
+  production: true,
+  apiUrl: 'https://api.tribecorner.fr/',
+  firebaseConfig : {
+      apiKey: "AIzaSyDuNMLuciC1INNmhzMEhlb5VWStT9J7Mu4",
+      authDomain: "familyhub-33bd5.firebaseapp.com",
+      projectId: "familyhub-33bd5",
+      storageBucket: "familyhub-33bd5.appspot.com",
+      messagingSenderId: "874615286570",
+      appId: "1:874615286570:web:bfff3469e09088487e4c53"
+    },
 };

9929433272453634e912661405ebd804cf36ad0f
jguillevic <jguillevic.real@gmail.com>
2024-01-15 21:31:28 +0100

    Suppression des espaces dans les brackets.

--- src/app/shopping-list/ui/component/add-item-shopping-list-form/add-item-shopping-list-form.component.ts
+++ src/app/shopping-list/ui/component/add-item-shopping-list-form/add-item-shopping-list-form.component.ts
@@ -1,16 +1,16 @@
-import { Component, EventEmitter, Input, OnDestroy, Output, TrackByFunction } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { AbstractControl, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { ItemShoppingList } from '../../../model/item-shopping-list.model';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
-import { SuggestedItemShoppingList } from '../../../model/suggested-item-shopping-list';
-import { Observable, Subject, of, switchMap, takeUntil, tap } from 'rxjs';
-import { TranslocoModule, TranslocoService, provideTranslocoScope } from '@ngneat/transloco';
-import { SuggestedItemShoppingListAutoCompleteService } from '../../../service/suggested-item-shopping-list-auto-complete.service';
-import { ItemShoppingListCategoryApiService } from '../../../service/item-shopping-list-category-api.service';
+import {Component, EventEmitter, Input, OnDestroy, Output, TrackByFunction} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {AbstractControl, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {ItemShoppingList} from '../../../model/item-shopping-list.model';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {SuggestedItemShoppingList} from '../../../model/suggested-item-shopping-list';
+import {Observable, Subject, of, switchMap, takeUntil, tap} from 'rxjs';
+import {TranslocoModule, TranslocoService, provideTranslocoScope} from '@ngneat/transloco';
+import {SuggestedItemShoppingListAutoCompleteService} from '../../../service/suggested-item-shopping-list-auto-complete.service';
+import {ItemShoppingListCategoryApiService} from '../../../service/item-shopping-list-category-api.service';
 
 @Component({
 selector: 'app-add-item-shopping-list-form',

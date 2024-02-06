f6fc7ea054cb91d9e3202941a63bc12f7ca14e0a
jguillevic <guillevicjerome@yahoo.fr>
2024-02-06 12:52:09 +0100

    Utilisation du PlaceholderItem dans le placeholder des listes.

--- src/app/shopping-list/ui/component/shopping-list-card-placeholder/shopping-list-card-placeholder.component.ts
+++ src/app/shopping-list/ui/component/shopping-list-card-placeholder/shopping-list-card-placeholder.component.ts
@@ -1,16 +1,18 @@
 import {Component, Input} from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {NgxSkeletonLoaderModule} from 'ngx-skeleton-loader';
+import { PlaceholderItemComponent } from "../../../../common/placeholder/placeholder-item/placeholder-item.component";
 
 @Component({
     selector: 'app-shopping-list-card-placeholder',
     standalone: true,
+    templateUrl: './shopping-list-card-placeholder.component.html',
+    styleUrl: `shopping-list-card-placeholder.component.scss`,
     imports: [
         MatCardModule,
-        NgxSkeletonLoaderModule
-    ],
-    templateUrl: './shopping-list-card-placeholder.component.html',
-    styleUrl: `shopping-list-card-placeholder.component.scss`
+        NgxSkeletonLoaderModule,
+        PlaceholderItemComponent
+    ]
 })
 export class ShoppingListCardPlaceholderComponent {
     @Input() public showActions: boolean = true;
